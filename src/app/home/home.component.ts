b9a9ee8d654cd9ebc603c754f6bb20f84cca9ab5
Sang Nguyen <nnsang24@gmail.com>
2020-08-10 15:47:57 +0700

    shareReplay operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { interval, noop, Observable, of, throwError, timer } from 'rxjs';
+import {
+	catchError,
+	delay,
+	delayWhen,
+	finalize,
+	map,
+	retryWhen,
+	shareReplay,
+	tap,
+} from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+	selector: 'home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+	beginnerCourses$: Observable<Course[]>;
 
+	advancedCourses$: Observable<Course[]>;
 
-    constructor() {
+	constructor() {}
 
-    }
+	ngOnInit() {
+		const http$ = createHttpObservable('/api/courses');
+		const courses$ = http$.pipe(
+			tap(() => console.log('HTTP request executed')),
+			map((res) => Object.values(res['payload'])),
+			shareReplay()
+		);
 
-    ngOnInit() {
-
-
-
-    }
-
+		this.beginnerCourses$ = courses$.pipe(
+			map((courses: Course[]) =>
+				courses.filter((course) => course.category === 'BEGINNER')
+			)
+		);
+		this.advancedCourses$ = courses$.pipe(
+			map((courses: Course[]) =>
+				courses.filter((course) => course.category === 'ADVANCED')
+			)
+		);
+	}
 }
